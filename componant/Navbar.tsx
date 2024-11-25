17f2bb76e005624e5071fd6a2053f34234cee2a9
AbdulSubhan145 <abduls12456@gmail.com>
2024-11-25 15:20:40 +0500

    first commit

---
+++ componant/Navbar.tsx
@@ -0,0 +1,14 @@
+
+export default function Navbar() {
+    return (
+      <>
+        <div className="navbar">
+          <ul className="navbar_list">
+            <li className="link"><a href="#">Work</a></li>
+            <li className="link"><a href="#">Blog</a></li>
+            <li className="link"><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </>
+    );
+  }
